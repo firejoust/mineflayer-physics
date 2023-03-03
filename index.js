aec8c66ad8bc99b7c7ebbe98ae3846729170039f
firejoust <42525540+firejoust@users.noreply.github.com>
2023-03-03 19:19:59 +1100

    almost implemented we cannot get the entity's current velocity \(server side\) so we will need to implement our own velocity

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Minecraft   = require("minecraft-data")
 const Motion      = require("./src/inject/motion")
 const Angle       = require("./src/utils/angle")
 
+const States = [ "back", "left", "forward", "right" ]
+
 module.exports.plugin = function inject(bot) {
     bot.physics.api = new Plugin(bot)
     Motion.inject(bot)
@@ -52,16 +54,52 @@ class Plugin {
     }
 
     getControls(entity) {
+        const controls = new ControlState()
+
+        /*
+            problem: we cannot see other entity's velocity.
+        */
+
+        // initialise the simulated player state
         const player = new Player()
         const state = fromLastState(this.bot.majorVersion, entity, new ControlState())
         this.physics.simulatePlayer(state, this.bot.world).apply(player)
 
-        // get angle between the velocity difference
-        const diff1 = entity.velocity.minus(player.entity.velocity)
-        const angle1 = Math.atan2(diff1.x, diff1.z)
-        const angle2 = entity.lastState.yaw
+        // get difference between simulated velocity vs. real velocity
+        const diff = player.entity.velocity.minus(entity.velocity)
+        const diffAngle = Math.atan2(diff.x, diff.z)
 
-        console.log(`angle1: ${angle1 * 180/Math.PI}, angle2: ${angle2 * 180/Math.PI}`)
-        console.log(`diff: ${Angle.difference(angle1, angle2) * 180/Math.PI}`)
+        // jump launch
+        controls["jump"] = (entity.position.y - player.entity.position.y) > 0
+
+        console.log(diffAngle)
+
+        // difference between simulated/actual velocity
+        if (diffAngle !== 0) {
+            const angle0 = Angle.difference(diffAngle, entity.lastState.yaw)
+            const angle1 = Angle.inverse(angle0)
+
+            // control states separated by 90 degrees
+            const radius = (Math.PI / 2) * 1.1 // add a bit of tolerance
+            let index = 0
+
+            // create a radius for each cardinal direction
+            for (let i = -Math.PI; i < Math.PI; i += Math.PI/2) {
+                // original angle radius
+                let x0, x1
+                x0 = i - radius / 2
+                x1 = i + radius / 2
+                // enable control state if destination angle within radius
+                x0 <= angle0 && angle0 <= x1 || x1 <= angle0 && angle0 <= x0 ||
+                x1 <= angle1 && angle1 <= x0 || x0 <= angle1 && angle1 <= x1
+                ? controls[States[index]] = true
+                : controls[States[index]] = false
+                // next state
+                index++
+            }
+        }
+
+        console.log(controls)
+        return controls
     }
 }
